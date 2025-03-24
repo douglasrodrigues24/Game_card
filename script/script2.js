23047f71d2cffaceab5590c53a1ae3b1fb2cf621
Douglas Rodrigues <131279606+douglasrodrigues24@users.noreply.github.com>
2025-03-24 14:48:11 -0300

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -2,14 +2,14 @@
 
 function btn_continue() {
     const btn = document.getElementById('continue').classList.add('sumir_btn');
-    const musicFim = new Audio('../audio/music_bad.mp3');
+    const musicFim = new Audio('audio/music_bad.mp3');
     musicFim.loop = true;
     musicFim.play();
     setTimeout(()=>{
         document.getElementById('body').classList.add('ceuRed');
         document.getElementById('h1').style.display = 'block';
         document.getElementById('gabi_boss').style.display = 'block';
-        const risada = new Audio('../audio/risada_maligna.mp3');
+        const risada = new Audio('audio/risada_maligna.mp3');
         risada.loop = true;
         risada.play();
     },4000)
